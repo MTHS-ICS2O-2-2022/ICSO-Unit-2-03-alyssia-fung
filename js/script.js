f7200a437edb521550bff7149fc3a299eae65aef
alyssia-fung <124389558+alyssia-fung@users.noreply.github.com>
2023-03-21 17:01:21 +0000

    stree with a JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Alyssia fung All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Alyssia fung
+// Created on: mar 2023
 // This file contains the JS functions for index.html
+
+/**
+ * This function gets users first name and age and shows it back to user.
+ */
+function enterClicked() {
+  // input
+  const streetNumber = document.getElementById("street-number").value
+  const streetName = document.getElementById("street-name").value
+
+  // output
+  document.getElementById("address").innerHTML =
+    "Your info is: " + streetNumber + ", " + streetName + "."
+}
